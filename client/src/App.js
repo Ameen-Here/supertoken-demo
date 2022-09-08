236a541e0336d6bf400e7f3bd83edc3beba7230d
Ameen-Here <johnharry125@yahoo.com>
2022-09-08 22:07:45 +0530

    Connected express server with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 import Homepage from "./components/Homepage";
 
+export function getApiDomain() {
+  const apiPort = process.env.REACT_APP_API_PORT || 3001;
+  const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+  return apiUrl;
+}
+
+export function getWebsiteDomain() {
+  const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+  const websiteUrl =
+    process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+  return websiteUrl;
+}
+
 function App() {
+  const apiClickHandler = async () => {
+    const data = await fetch("/api/hello");
+    console.log(data);
+    const printData = await data.json();
+    console.log(printData);
+  };
   return (
     <>
       <Homepage />
+      <button onClick={apiClickHandler}>Click for API</button>
     </>
   );
 }
