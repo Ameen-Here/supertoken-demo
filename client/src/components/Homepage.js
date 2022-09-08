efb5f0aa90ef5f76cf363937f48778bcde78d841
Ameen-Here <johnharry125@yahoo.com>
2022-09-08 21:17:36 +0530

    Added a server

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <h2>Welcome to supertokens demo login/signup</h2>
+    </div>
+  );
+};
+
+export default Homepage;
